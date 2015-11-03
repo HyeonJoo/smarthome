ba02a1e3fe565f7ea8ccc585d8ed268971a945da
HyeonJoo <hyeonjoopark88@gmail.com>
2015-11-04 02:28:59 +0900

    Test Ajax

---
+++ AjaxTest/controldb.php
@@ -0,0 +1,25 @@
+<?php
+// $servername = "localhost";
+// $username = "root";
+// $password = "q1w2e3r4";
+// $dbname = "smarthome"
+
+echo "$_POST[item]";
+// Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } 
+
+// $sql="INSERT INTO control (item) VALUES ('$_POST[item]' )";
+
+// if (!mysql_query($sql,$con))
+// {
+//     die('Error: ' . mysql_error());
+// }
+
+// mysql_close($con)
+
+// header('Location: /control.php');
+?>
